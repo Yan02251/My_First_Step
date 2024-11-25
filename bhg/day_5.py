e6afae7f6ffd4c6e28aca6c0a85b160774957954
박현기 <baghyeongi@baghyeongiui-MacBookPro.local>
2024-11-25 18:50:59 +0900

    과제... 다 하지는 못함.

---
+++ bhg/day_5.py
@@ -0,0 +1,30 @@
+'''
+
+print("오늘도 손이 너무나 바빠핑")
+print("어떻게하면 내일 쉴 수있을까?")
+
+print("오늘도 손이 너무나 바빠핑")
+print("어떻게하면 내일 쉴 수있을까?")
+
+print("오늘도 손이 너무나 바빠핑")
+print("어떻게하면 내일 쉴 수있을까?")
+
+def busy_p(body, days):
+    print(f"오늘도 {body}이 너무나 바빠핑")
+    print(f"어떻게하면 {days} 쉴 수있을까?")
+
+
+busy_p("발", "내일 모레")
+
+'''
+
+hello = "안녕하세요"
+
+def oz_len(hello):
+    count = 0
+    for i in hello:
+        count += 1
+    return count
+
+
+print (oz_len(hello))
